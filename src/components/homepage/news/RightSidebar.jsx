a9fda54b5edf29d3342e72c605401241b60559f7
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-05-02 23:27:10 +0600

    social sign In google and github

--- src/components/homepage/news/RightSidebar.jsx
+++ src/components/homepage/news/RightSidebar.jsx
@@ -1,16 +1,34 @@
+"use client";
+import { authClient } from "@/lib/auth-client";
 import React from "react";
 import { FaGithub, FaGoogle } from "react-icons/fa";
 
 const RightSidebar = () => {
+  const handleGoogleSignIn = async () => {
+    const data = await authClient.signIn.social({
+      provider: "google",
+    });
+    console.log("Google Sign In: ", data);
+  };
+
+  const handleGithubSignIn = async () => {
+    const data = await authClient.signIn.social({
+      provider: "github",
+    });
+    console.log("Github SignIn: ", data);
+  };
   return (
     <div>
       <h2 className="font-bold text-lg mb-4">Login with</h2>
       <div className="flex flex-col gap-2">
-        <button className="btn border-blue-500 text-blue-500">
+        <button
+          className="btn border-blue-500 text-blue-500"
+          onClick={handleGoogleSignIn}
+        >
           <FaGoogle />
           Login with google
         </button>
-        <button className="btn">
+        <button className="btn" onClick={handleGithubSignIn}>
           <FaGithub />
           Login with github
         </button>
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,7 +2,6 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-
 import dns from "node:dns";
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
@@ -14,7 +13,17 @@ export const auth = betterAuth({
     // Optional: if you don't provide a client, database transactions won't be enabled.
     client,
   }),
-   emailAndPassword: { 
-    enabled: true, 
-  }, 
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId:process.env.GOOGLE_CLIENT_ID,
+      clientSecret:process.env.GOOGLE_CLIENT_SECRET,
+    },
+     github: { 
+            clientId: process.env.GITHUB_CLIENT_ID, 
+            clientSecret: process.env.GITHUB_CLIENT_SECRET , 
+        }, 
+  },
 });
